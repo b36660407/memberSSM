e0a42452494a9f284f39eca1fd24f13a15dc2600
b36660407 <b36660407@gmail.com>
2024-05-16 19:56:12 +0800

    0516_v1.0
    SSM架構練習

---
+++ src/main/java/dao/DbConnection.java
@@ -0,0 +1,41 @@
+package dao;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.session.SqlSession;
+
+public class DbConnection {
+
+	public static void main(String[] args) {
+		System.out.println(DbConnection.getDb());
+
+	}
+	
+	
+	public static SqlSession getDb()
+	{
+		SqlSession session=null;
+		try {
+			//Reader r=Resources.getResourceAsReader("mybatis-config.xml");
+			InputStream r=Resources.getResourceAsStream("mybatis-config.xml");
+			SqlSessionFactory sf=new SqlSessionFactoryBuilder().build(r);
+			session=sf.openSession();
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		return session;
+		
+	}
+
+}
---
+++ src/main/java/service/MemberService.java
@@ -0,0 +1,11 @@
+package service;
+
+import vo.Member;
+
+public interface MemberService {
+	//create
+	void addMember(Member m);
+	
+	//read
+	String showAll();
+}
